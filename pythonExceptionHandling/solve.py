1d88a62143af2b518becc4a23a0190473bc1d4bc
REZ-OAN <ahmedabir.rez@gmail.com>
2023-09-04 00:58:02 +0600

    version-2023-09-04 12:58:02 AM added userDefined Exception on pythonExceptionHandling

--- pythonExceptionHandling/solve.py
+++ pythonExceptionHandling/solve.py
@@ -1,16 +1,16 @@
 
-# x,y = [int(i) for i in input().split()]
+x,y = [int(i) for i in input().split()]
 
-# try :
-#     z = x/y
-# except Exception as e :
-#     print("Exception Occured!!! ", e)
-#     ## to know the name of that error
-#     print("nametype : ",type(e).__name__)
-#     z = None
-#     print("Z : ",z)
-# else :
-#   print("Division Is : ", z)
+try :
+    z = x/y
+except Exception as e :
+    print("Exception Occured!!! ", e)
+    ## to know the name of that error
+    print("nametype : ",type(e).__name__)
+    z = None
+    print("Z : ",z)
+else :
+  print("Division Is : ", z)
  
 ## custom or user defined error handling
 
